42a975f296667d547c6975f0521abd84c38cb884
LRache <2746324475@qq.com>
2024-10-18 18:16:36 +0800

    feat: set layout

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,38 +1,47 @@
 import "./GameBoard.css"
 import {useEffect} from "react";
 
+const margin = 20
+const blockSize = 100
+
 const blockBasicStyles = {
-    width:  "100px",
-    height: "100px",
+    width:  blockSize + "px",
+    height: blockSize + "px",
     borderRadius: "6px",
 
     textAlign: "center",
     lineHeight: "100px",
     userSelect: "none",
+
+    position: "absolute",
 }
 
 const BlockStyle ={
+    0: {textColor: ""       , backgroundColor: "#CDC1B4"},
     2: {textColor: "#776E65", backgroundColor: "#EEE4DA"},
-    4: {textColor: "#F9F6F2", backgroundColor: "#EDE0C8"},
+    4: {textColor: "#F9F6F2", backgroundColor: "#EDE0C8"}
 }
 
-function genBlockStyle(number) {
+function genBlockStyle(number, loc) {
+    const row = Math.floor(loc / 4)
+    const col = loc % 4
     return {
         ...blockBasicStyles,
         ...BlockStyle[number],
+        top: margin + row * (margin + blockSize)
     }
 }
 
-function GameBlock({number}) {
+function GameBlock({number, loc}) {
     return (
-        <div className="game-block mt-2 border-2 shadow-sm" style={genBlockStyle(number)}>
+        <div style={genBlockStyle(number, loc)}>
             {number === 0 ? "" : number}
         </div>
     )
 }
 
 function GameBoard({numbers, spawnAnimation}) {
-    const blocks = numbers.map(n => <GameBlock number={n}/>)
+    const blocks = numbers.map(n => <GameBlock number={n} loc={n}/>)
 
     useEffect(() => {
 
